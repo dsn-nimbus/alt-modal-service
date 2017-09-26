3699f95d177da4a911fe9eb927cbd4f1992fb892
Eric Mendes Dantas <ericdantas0@gmail.com>
2017-09-26 11:58:09 -0300

    .

--- src/alt-modal-service.js
+++ src/alt-modal-service.js
@@ -1,23 +1,22 @@
-;(function() {
+;(function(ng) {
   'use strict';
 
-  angular
-    .module('alt.modal-service', [])
+  ng.module('alt.modal-service', [])
     .service('AltModalService', [function() {
-      this.close = function(id, opts) {
-        if (angular.isUndefined(id)) {
-            throw new TypeError('O id do modal deve ser informado para que o mesmo seja fechado.');
-        }
-
-        $(id).modal(opts || 'hide');
-      };
-
       this.open = function(id, opts) {
-        if (angular.isUndefined(id)) {
+        if (ng.isUndefined(id)) {
             throw new TypeError('O id do modal deve ser informado para que o mesmo seja aberto.');
         }
 
         $(id).modal(opts || 'show');
       };
+
+      this.close = function(id, opts) {
+        if (ng.isUndefined(id)) {
+            throw new TypeError('O id do modal deve ser informado para que o mesmo seja fechado.');
+        }
+
+        $(id).modal(opts || 'hide');
+      };
     }]);
-}());
+}(angular));
